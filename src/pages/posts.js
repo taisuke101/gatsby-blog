b77818da587cb9cc19291768b939d55e904b60ff
taisuke <uwattotaitai@gmail.com>
2020-07-17 21:29:22 +0900

    記事一覧ページの作成

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -5,11 +5,39 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import Posts from '../components/Posts'
 
-// ...GatsbyImageSharpFluid
+
 const PostsPage = ({ data }) => {
-  return <>
+  const {allMdx:{nodes:posts}} = data
+  return <Layout>
     <Hero />
-  </>
+    <Posts posts={posts} title="記事一覧" />
+  </Layout>
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "YYYY年MM月DD日")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
 
 export default PostsPage
--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import Posts from '../components/Posts'
-import { graphql } from 'gatsby'
 
-const CategoryTemplate = () => {
-  return <h2>categories template</h2>
+
+const CategoryTemplate = props => {
+  const {
+    pageContext:{category}
+  } = props
+  const {
+    data:{
+      categories:{nodes:posts}
+    }
+  } = props
+  return <Layout>
+    <Hero />
+    <Posts posts={posts} title={`category / ${category}`}/>
+  </Layout>
 }
+
+export const query = graphql`
+  query GetCategories($category: String){
+    categories:allMdx(sort: {fields: frontmatter___date, order: DESC}, 
+      filter: {frontmatter: {category: {eq: $category}}}) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "YYYY年 MM月 DD日")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
 
 export default CategoryTemplate
